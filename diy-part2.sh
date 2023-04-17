a5f6bad34dc3a4ded5050b18573a47ad18247ee9
lovezanky <31689073+lovezanky@users.noreply.github.com>
2023-04-17 22:59:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#后面是新加的
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 # Modify default PassWord
